48f8661b3ce6486b6c5e72e02670f0764b94da25
saber <saber.tabatabaee@gmail.com>
2021-08-01 18:23:59 +0430

    init

---
+++ resources/views/saber.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>saber</title>
+
+</head>
+<body class="saberbody">
+    saber
+</body>
+
+<style>
+</style>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/saber', function () {
+    return view('saber');
+});
+
+Route::get('/1', function () {
+    return view('layouts.app');
+});
+
+
+Route::get('/2', function () {
+    return view('welcome');
+});
+
+
+Route::get('/home', function () {
+    return view('welcome');
+});
